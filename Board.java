49df4bf741007f65b0740435c47178b19e29f46a
V Lam <goldranger3033@gmail.com>
2017-03-08 08:43:14 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,1 +1,21 @@
-
+public class Board
+{
+  int[] board = new int[15];
+  for(int pegs: board)
+  {
+    pegs = 1;
+  }
+  public void resetBoard()
+  {
+    for(int pegs: board)
+    {
+      pegs = 1;
+    }
+  }
+  public int setPeg(start, mid, end)
+  {
+    board[start] = 0;
+    board[mid] = 0;
+    board[end] = 1;
+  }
+}
