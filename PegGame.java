715ca05fd6744e8ccffc04bed4f422ae69f1c67a
WinterScout <Lordsmurflover610621714plzno@gmail.com>
2017-03-08 08:47:54 -0600

    Update PegGame.java

--- PegGame.java
+++ PegGame.java
@@ -2,5 +2,13 @@ public class PegGame
 {
   public static void main(String[] args)
   {
+    public boolean checkKonami()
+    {
+      
+    }
+    public void loadGraphics()
+    {
+      
+    }
   }
 }
