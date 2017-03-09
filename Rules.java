d13b539508cf01b6fc5832334e7911fbce65a32f
WinterScout <Lordsmurflover610621714plzno@gmail.com>
2017-03-09 07:48:01 -0600

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -3,8 +3,44 @@ public class Rules
   public boolean checkWinner(Board)
   {
     boolean onePeg = false;
+    int counter = 0;
     for(int i = 0; i < Board.getBoard().length; i++)
     {
+      if(Board.getPeg(i) == 1)
+      {
+         counter++
+      }
     }
+    if(counter == 1)
+    {
+      onePeg = true; 
+    }
+    else
+    {
+      onePeg = false; 
+    }
+    return onePeg;
+  }
+  public boolean checkLoser(Board)
+  {
+    boolean moreThanOne = false;
+    int counter = 0;
+    for(int i = 0; i < Board.getBoard().length; i++)
+    {
+      if(Board.getPeg(i) == 1)
+      {
+         counter++
+      }
+    }
+    if(counter != 1)
+    {
+      moreThanOne = false; 
+    }
+    else
+    {
+      moreThanOne = true; 
+    }
+    return moreThanOne;
+    
   }
 }
