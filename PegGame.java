cc7ce534c109f3c442a397e61e79754a46ba4c6b
V Lam <goldranger3033@gmail.com>
2017-03-10 09:03:29 -0600

    Update PegGame.java

--- PegGame.java
+++ PegGame.java
@@ -11,6 +11,20 @@ public class PegGame
 {
   public static void main(String[] args)
   {
+    JFrame boardFrame = new JFrame()
+    JFrame pegBoard = new JFrame();
+    GridLayout pyramid = new GridLayout(5, 1);
+    JFrame level1 = new JFrame();
+    JFrame level2 = new JFrame();
+    GridLayout onextwo = new GridLayout(1, 2);
+    JFrame level3 = new JFrame();
+    GridLayout onexthree = new GridLayout(1, 3);
+    JFrame level4 = new JFrame();
+    GridLayout onexfour = new GridLayout(1, 4);
+    JFrame level5 = new JFrame();
+    GridLayout onexfive = new GridLayout(1, 5);
+    
+    
     public boolean checkKonami()
     {
       
@@ -19,5 +33,8 @@ public class PegGame
     {
       
     }
+    boardFrame.add(pegGame, BorderLayout.CENTER);
+    boardFrame.add(title, BorderLayout.NORTH);
+    boardFrame.add(resetButton, BorderLayout.SOUTH);
   }
 }
