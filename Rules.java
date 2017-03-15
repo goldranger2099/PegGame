4333392adc2178d6c0ee9ec67aac47882ababddd
V Lam <goldranger3033@gmail.com>
2017-03-15 08:17:54 -0500

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -25,26 +25,27 @@ public class Rules
   //Checks to see if there are any moves left.
   public boolean checkLoser(Board board)
   {
-    boolean moreThanOne = false;
     int counter = 0;
     for(int i = 0; i < board.getBoard().size(); i++)
     {
       if(board.get(i).getValue() == 1)
       {
-         counter++;
+        if(canMove(board, i + 1) > 0)
+        {
+          counter++;
+        }
       }
     }
-    if(counter != 1)
+    if(counter >= 1)
     {
-      moreThanOne = false; 
+      return false; 
     }
     else
     {
-      moreThanOne = true; 
+      return true; 
     }
-    return moreThanOne;
   }
-  //Cchecks to see whether a peg can move to a certain space or not.
+  //Checks to see whether a peg can move to a certain space or not.
   public int canMove(Board board, int peg)
   {
     int counter = 0;
