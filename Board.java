8043ad8ccb54d29c70f8361b3b2380f1c1a74b03
WinterScout <Lordsmurflover610621714plzno@gmail.com>
2017-03-16 08:28:46 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,40 +3,6 @@ import java.util.ArrayList;
 public class Board
 {
   private ArrayList<Peg> board = new ArrayList<>();
-  class Peg
-  {
-    /*
-    Sets the X and Y of the Peg board as well as the beginning values.
-    */
-    private int posX;
-    private int posY;
-    private int value;
-    public Peg(int posX, int posY)
-    {
-      this.posX = posX;
-      this.posY = posY;
-    }
-    //Returns X
-    public int getPosX()
-    {
-      return posX;
-    }
-    //Returns Y
-    public int getPosY()
-    {
-      return posY;
-    }
-    //Changes the Value in that pegs properties
-    public void setValue(int value)
-    {
-      this.value = value;
-    }
-    //Returns a pegs value
-    public int getValue()
-    {
-      return value;
-    }
-  }
   public Board()
   {
     //Creation of Pegs and sets them on board.
