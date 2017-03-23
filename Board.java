69055116a501c4453c96a0fbdc84b778ac3b4ccb
Evan Dawkins <edawk10@gmail.com>
2017-03-23 08:22:56 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,13 +1,17 @@
+/**
+ * The board class that holds all the pegs
+ * @author Robert Vince Evan and Nolan
+ */
 import java.util.ArrayList;
 
 public class Board
 {
   private ArrayList<Peg> board = new ArrayList<>();
+  /**
+   * Constructor for the peg positions
+   */
   public Board()
   {
-    /**
-    * Creation of Pegs and sets them on board.
-    */
     Peg peg1 = new Peg(5, 5, 1);
     board.add(peg1);
     Peg peg2 = new Peg(4, 4, 2);
@@ -38,9 +42,9 @@ public class Board
     board.add(peg14);
     Peg peg15 = new Peg(9, 1, 15);
     board.add(peg15);
-    /**
-    * Sets ALL pegs to 1, which means the pegs are on the board.
-    */
+    
+    //Sets ALL pegs to 1, which means the pegs are on the board.
+    
     for(int i = 0; i < board.size(); i++)
     {
       board.get(i).setValue(1);
@@ -56,7 +60,10 @@ public class Board
       board.get(i).setValue(1);
     }
   }
-  //Changes a peg to zero, which means the peg is removed.
+  /**
+   * Changes a peg to zero, which means the peg is removed.
+   * @param peg Peg Number
+   */
   public void removePeg(int peg)
   {
     if(peg > 0 && peg < 16)
@@ -64,7 +71,12 @@ public class Board
       board.get(peg - 1).setValue(0);
     }
   }
-  //Moves a peg from x - 2, x + 2, y + 2, or y - 2. The starting peg jumps to the empty end position, which removes the peg in the middle
+  /**
+   * Moves a peg from x - 2, x + 2, y + 2, or y - 2. The starting peg jumps to the empty end position, which removes the peg in the middle
+   * @param start Starting peg number
+   * @param mid Middle peg number
+   * @param end End peg number
+   */
   public void movePeg(int start, int mid, int end)
   {
     if (board.get(start).getValue() == 1 && board.get(mid).getValue() == 1 && board.get(end).getValue() == 0)
@@ -74,7 +86,12 @@ public class Board
       board.get(end - 1).setValue(1);
     }
   }
-  //Finds peg in given X and Y spot
+  /**
+   * Finds peg in given X and Y spot
+   * @param posX Position x of the peg
+   * @param posY Position y of the peg
+   * @return Peg A peg with given position
+   */
   public Peg getPeg(int posX, int posY)
   {
     for(int i = 0; i < board.size(); i++)
@@ -86,7 +103,10 @@ public class Board
     }
     return null;
   }
-  //Returns board
+  /**
+   * Returns board
+   * @return ArrayList<Peg> An array list containing pegs
+   */
   public ArrayList<Peg> getBoard()
   {
     return board;
